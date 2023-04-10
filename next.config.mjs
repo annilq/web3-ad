d3f772785ffabe72a17e5976d2bd0e8bcf179e7a
annilq <410493517@qq.com>
2023-04-10 10:08:38 +0800

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["avatars.githubusercontent.com", "images.unsplash.com"],
+  },
   experimental: {
+    appDir: true,
     fontLoaders: [
       {
         loader: "@next/font/google",
